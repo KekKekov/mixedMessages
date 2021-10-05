15ffe849597320dd7f6e8b3efaf8958108e2cf74
Kek Kekov <kuzminaliza@gmail.com>
2021-10-05 14:11:58 +0200

    Small fixes to code

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const generateMessage = () => {
         activities: ["going for a walk", "taking photographs", "taking a bath", "siting in the sun", "doing some yoga", "swimming", "buying fresh flowers", "cooking", "jogging", "meditating", "enjoying a cup of tea"],
         music: ["Radiohead", "A Perfect Circle", "Alt-j", "Lana del Rey", "Ludovico Eunaudi", "Glass Animals", "The Neighbourhood", "Doja Cat", "Deftones"]
     };
-    const randEmoji = Math.floor(Math.random() * words.emojis.length);
-    const randActivity = Math.floor(Math.random() * words.activities.length);
-    const randMusic = Math.floor(Math.random() * words.music.length);
+    const numRandEmoji = Math.floor(Math.random() * words.emojis.length);
+    const numRandActivity = Math.floor(Math.random() * words.activities.length);
+    const numRandMusic = Math.floor(Math.random() * words.music.length);
 
-    const emoji = words.emojis[randEmoji];
-    const activity = words.activities[randActivity];
-    const band = words.music[randMusic];
+    const emoji = words.emojis[numRandEmoji];
+    const activity = words.activities[numRandActivity];
+    const band = words.music[numRandMusic];
     
-    let phrase = `Mood of the day: ${emoji}. What about ${activity} while listening to ${band} to make your day better?`
+    let phrase = `Mood of the day: ${emoji}. What about ${activity} while listening to ${band} to make your day better?`;
     return phrase;
 }
 
